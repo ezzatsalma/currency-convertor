e9f317540a84706e30ca7c9f908c00b302ea90b2
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-04-06 23:30:49 +0500

    programming

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env Node
+import inquirer from "inquirer";
+const country:any={
+    USD:1,
+    PKR:280,
+    INR:74,
+    KRW:1280,
+    JPY:234,
+
+}
+let currency= await inquirer.prompt([{
+    name:"first",
+    message:"select from",
+    type:"list",
+    choices:["PKR","INR","KRW","USD","JPY"]
+},
+{
+    name:"second",
+    type:"list",
+    message:"choose to",
+    choices:["PKR","INR","KRW","USD","JPY"]
+},
+{
+    name:"amount",
+    type:"input",
+    message:"write amount"
+
+}
+])
+let fromvalue= country [currency.first]
+let tovalue=country[currency.second]
+let amt=currency.amount
+ let basevalue= amt/fromvalue
+let convertedvalue= basevalue*tovalue
+console.log(convertedvalue.toFixed(2))
